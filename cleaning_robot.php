a3dec01d21d78c1da1efc2dc70f8e67bf07ae977
Angga Lingga <aslingga@gmail.com>
2018-10-24 16:02:01 +0700

    now can read file from php argv[i]

---
+++ cleaning_robot.php
@@ -0,0 +1,27 @@
+<?php
+    include_once 'Robot.php';
+    include_once 'Floor.php';
+    
+    $source = $argv[1];
+    $destination = $argv[2];
+    
+    $source = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . $source);
+    
+    $sourceArr = json_decode($source, true);    
+    $map = $sourceArr['map'];
+    $start = $sourceArr['start'];
+    $instructions = $sourceArr['commands'];
+    $battery = $sourceArr['battery'];
+    
+    $robot = new Robot($start, $instructions, $battery);
+    $floor = new Floor();
+    
+    $floor->setSource();
+    $floor->initiateGrid($map);
+    
+    $robot->setFloor($floor);
+    $robot->startJob();
+    
+    $jsonDestination = json_encode($robot->getResult(), JSON_PRETTY_PRINT);
+    file_put_contents($destination, $jsonDestination);
+?>
